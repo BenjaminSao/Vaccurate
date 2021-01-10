d062cd79942ae850ac9f7908d262f86071c95b70
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2021-01-10 14:17:41 -0500

    Changed overview.js

--- src/overview/overview.js
+++ src/overview/overview.js
@@ -3,20 +3,34 @@ var email = sessionStorage.getItem("email")
 
 var usersRef = db.collection("users");
 var clinicsRef = db.collection("clinics").doc(email);
-var clinicObj;
 var doses;
 
-// usersRef = usersRef.orderBy("score").limit();
 
 clinicsRef.get().then(function(doc) {
     if (doc.exists) {
-        doses = doc.data().doses;
-        console.log(doses);
+        var list = document.createElement('ul');
+        this.doses = doc.data().doses;
+        usersRef.orderBy("score", "desc").limit(this.doses).get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                console.log(doc.data());
+                var score = document.createElement('li');
+                score.appendChild(document.createTextNode("score: " + doc.data().score));
+                list.appendChild(score);
+                var name = document.createElement('li');
+                name.appendChild(document.createTextNode("name: " + doc.data().first + " " + doc.data().last));
+                list.appendChild(name);
+                var phone = document.createElement('li');
+                phone.appendChild(document.createTextNode("phone: " + doc.data().phone));
+                list.appendChild(phone);
+                var email = document.createElement('li');
+                email.appendChild(document.createTextNode("email: " + doc.data().email));
+                list.appendChild(email);
+            });
+        });
+        document.getElementById("list").appendChild(list);
     } else {
         console.log("No such document!");
     }
 }).catch(function(error) {
     console.log("Error getting document:", error);
 });
-
-console.log(doses);
